ba62b2a86a3077078eadf1af89d5eab86e366af3
Connor Briggs <cgbriggs99@gmail.com>
2023-07-18 19:39:39 -0400

    Added mcfpp

---
+++ repo-scripts/mcfpp/version.py
@@ -0,0 +1,11 @@
+"""
+mcfpp.version
+
+Contains a single value, __VERSION, which tells the version. This should be
+accessed using the function mcfpp.version.version().
+"""
+
+__VERSION = "alpha"
+
+def version() :
+    return __VERSION
